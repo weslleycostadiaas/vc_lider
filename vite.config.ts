0d5e1e89861012d684db871b018bd988f34f84e7
Weslley da Costa Dias <weslleydacostadias@yahoo.com.br>
2026-09-22 10:09:33 -0300

    Libera domínio radar no Vite
    Libera domínio radar no Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: ['radar.julienesalvan.com.br'],
+  },
+
   preview: {
     host: '0.0.0.0',
-    allowedHosts: [
-      'radar.julienesalvan.com.br',
-    ],
+    allowedHosts: ['radar.julienesalvan.com.br'],
   },
 })
