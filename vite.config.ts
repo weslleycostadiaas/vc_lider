4351c052128f65b31fd42618fb27cb3e519e46cf
Weslley da Costa Dias <weslleydacostadias@yahoo.com.br>
2026-09-22 11:58:15 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,8 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-<<<<<<< HEAD
-  plugins: [react()],
-
-  server: {
-    host: '0.0.0.0',
-    allowedHosts: ['radar.julienesalvan.com.br'],
-  },
-
-  preview: {
-    host: '0.0.0.0',
-    allowedHosts: ['radar.julienesalvan.com.br'],
-=======
   nitro: {
     preset: "node-server",
->>>>>>> fc2c5379c537bcd89a20136187a029a4834aee83
   },
 
   tanstackStart: {
